7a6d6c4fb9ec69f1bafc2198213c516865bba331
nirmit-gangani <nirmitgangani@gmail.com>
2021-06-29 03:50:54 -0700

    test

--- sketch.js
+++ sketch.js
@@ -24,14 +24,59 @@ function setup() {
 
   ground = new Ground(600,380,1200,20);
 
+  boggie1=new Boggie(50,170,50,50)
+  boggie2=new Boggie(150,170,50,50)
+  boggie3=new Boggie(250,170,50,50)
+  boggie4=new Boggie(350,170,50,50)
+  boggie5=new Boggie(450,170,50,50)
+  boggie6=new Boggie(550,170,50,50)
 
+  chain1=new Chain(boggie1.body,boggie2.body);
+  chain2=new Chain(boggie2.body,boggie3.body);
+  chain3=new Chain(boggie3.body,boggie4.body);
+  chain4=new Chain(boggie4.body,boggie5.body);
+  chain5=new Chain(boggie5.body,boggie6.body);
+
+  rock=new Rock(1100,200,100,100)
 }
 
 function draw() {
   background(bg);  
   Engine.update(myEngine);
 
+  boggie1.show();
+  boggie2.show();
+  boggie3.show();
+  boggie4.show();
+  boggie5.show();
+  boggie6.show();
  
+  chain1.show();
+  chain2.show();
+  chain3.show();
+  chain4.show();
+  chain5.show();
+
+  rock.show();
+
+  var colision=Matter.SAT.collides(boggie6.body,rock.body);
+
+  if(colision.collided){
+    flag=1;
+  }
+  if(flag===1){
+    textSize(30);
+    fill("blue");
+    text("CRASH",500,200);
+    crashSound.play();
+  }
   }
 
+  function keyPressed(){
+    if(keyCode===RIGHT_ARROW){
+      Matter.Body.applyForce(boggie6.body,{x: boggie6.body.position.x,y:boggie6.body.position.y},{x:0.5,y:0});
+      trainSound.play();
+
+    }
+  }
   
